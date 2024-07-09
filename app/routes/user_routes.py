aa98679ac8695fa6ece53328842c35474943a008
RamKishore <mramki1803@gmail.com>
2024-07-09 17:08:00 +0530

    getMatchedUsers

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import JSONResponse
 from prisma import Prisma
-from requests import request
 
 load_dotenv()
 
@@ -12,6 +11,7 @@ BACKEND_API = os.getenv('BACKEND_API')
 user_router = APIRouter()
 
 prisma = Prisma()
+
 
 @user_router.get('/users')
 async def get_all_usernames():
@@ -22,17 +22,47 @@ async def get_all_usernames():
     except Exception as e:
         print(f"Error fetching user names: {e}")
         raise HTTPException(status_code=500, detail="Error fetching user names")
+    finally:
+        await prisma.disconnect()
+
 
 @user_router.get('/likedUsers')
 async def get_liked_users(request: Request):
     await prisma.connect()
 
-    user_email = request.state.user['email']
-    user_id = (await prisma.users.find_unique(where={'email': user_email})).user_id
+    try:
+        user_email = request.state.user['email']
+        user_id = (await prisma.users.find_unique(where={'email': user_email})).user_id
 
-    liked_users = await prisma.likes.find_many(where={'user_id': user_id})
-    liked_user_id = [like.liked_user_id for like in liked_users]
+        liked_user_ids = [like.liked_user_id for like in await prisma.likes.find_many(where={'user_id': user_id})]
+        liked_users = await prisma.users.find_many(where={'user_id': {'in': liked_user_ids}})
 
-    return JSONResponse(
-        content={'user_id': user_id, 'liked_user_id': liked_user_id}
-    )
+        return JSONResponse(
+            content={'user_id': user_id, 'liked_users': liked_users}
+        )
+    finally:
+        await prisma.disconnect()
+
+
+@user_router.get('/matchedUsers')
+async def get_matched_users(request: Request):
+    await prisma.connect()
+
+    try:
+        user_email = request.state.user['email']
+        user_id = (await prisma.users.find_unique(where={'email': user_email})).user_id
+
+        matches = await prisma.matches.find_many(where={
+            'OR': [
+                {'user1_id': user_id},
+                {'user2_id': user_id}
+            ]
+        })
+        matched_user_ids = [match.user1_id if match.user2_id == user_id else match.user2_id for match in matches]
+        matched_users = await prisma.users.find_many(where={'user_id': {'in': matched_user_ids}})
+
+        return JSONResponse(
+            content={'user_id': user_id, 'liked_users': matched_users}
+        )
+    finally:
+        await prisma.disconnect()
