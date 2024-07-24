c973ec8ed9ed5a7198982cd277aebec80eef33ed
RamKishore <mramki1803@gmail.com>
2024-07-24 14:21:51 +0530

    used httpexception to throw error

--- app/routes/match_routes.py
+++ app/routes/match_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import JSONResponse
 from prisma import Prisma
 
@@ -6,56 +6,45 @@ match_router = APIRouter()
 
 prisma = Prisma()
 
-@match_router.post('/like/{liked_user_id}')
+@match_router.get('/like/{liked_user_id}')
 async def like_user(liked_user_id: int, request: Request):
     try:
         await prisma.connect()
 
         user_email = request.state.user['email']
-        user_id = (await prisma.users.find_unique(where={'email': user_email})).user_id
-        if liked_user_id == user_id:
-            return JSONResponse(
-                status_code=400,
-                content={'message': 'You cannot like yourself'}
-            )
-        elif liked_user_id not in [user.user_id for user in await prisma.users.find_many()]:
-            return JSONResponse(
-                status_code=404,
-                content={'message': 'User not found'}
-            )
-        if await prisma.likes.find_first(where={'user_id': user_id, 'liked_user_id': liked_user_id}):
-            return JSONResponse(
-                status_code=400,
-                content={'message': 'User already liked'}
-            )
+        user = await prisma.users.find_unique(where={'email': user_email})
         
+        if not user:
+            raise HTTPException(status_code=404, detail='User not found')
+
+        user_id = user.user_id
+        
+        if liked_user_id == user_id:
+            raise HTTPException(status_code=400, detail='You cannot like yourself')
+
+        liked_user = await prisma.users.find_unique(where={'user_id': liked_user_id})
+        if not liked_user:
+            raise HTTPException(status_code=404, detail='User not found')
+
+        existing_like = await prisma.likes.find_first(where={'user_id': user_id, 'liked_user_id': liked_user_id})
+        if existing_like:
+            raise HTTPException(status_code=400, detail='User already liked')
+
         await prisma.likes.create(data={'user_id': user_id, 'liked_user_id': liked_user_id})
-        print(f'liked_user_id: {liked_user_id}')
-        matched_user = await prisma.likes.find_first(
-            where={'user_id': liked_user_id, 'liked_user_id': user_id}
-        )
+        
+        matched_user = await prisma.likes.find_first(where={'user_id': liked_user_id, 'liked_user_id': user_id})
+        
         if matched_user:
-            await prisma.matches.create(data={
-                'user1_id': user_id,
-                'user2_id': liked_user_id
-            })
-            return JSONResponse(
-                status_code=200,
-                content={'message': 'It\'s a match!'}
-            )
-        elif matched_user is None:
-            return JSONResponse(
-                status_code=200,
-                content={'message': 'User liked successfully, We will notify you if it\'s a match'}
-            )
-        return JSONResponse(
-            status_code=200,
-            content={'message': 'User liked successfully, We will notify you if it\'s a match'}
-        )
+            await prisma.matches.create(data={'user1_id': user_id, 'user2_id': liked_user_id})
+            return JSONResponse(status_code=200, content={'message': 'It\'s a match!'})
+        
+        return JSONResponse(status_code=200, content={'message': 'User liked successfully, We will notify you if it\'s a match'})
+    
+    except HTTPException as http_exc:
+        return JSONResponse(status_code=http_exc.status_code, content={'message': http_exc.detail})
+    
     except Exception as e:
-        return JSONResponse(
-            status_code=500,
-            content={'message': 'Error liking user', 'error': str(e)}
-        )
+        return JSONResponse(status_code=500, content={'message': 'Error liking user', 'error': str(e)})
+    
     finally:
         await prisma.disconnect()
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -28,10 +28,19 @@ async def get_liked_users(request: Request):
         user_id = (await prisma.users.find_unique(where={'email': user_email})).user_id
 
         liked_user_ids = [like.liked_user_id for like in await prisma.likes.find_many(where={'user_id': user_id})]
-        liked_users = await prisma.users.find_many(where={'user_id': {'in': liked_user_ids}})
+        liked_users = await prisma.users.find_many(
+            where={'user_id': {'in': liked_user_ids}},
+        )
+        filtered_users = [
+            {
+            'user_id': user.user_id,
+            'profile_picture': user.profile_picture,
+            'name': user.name
+            }
+            for user in liked_users
+        ]
 
-        return liked_users
-
+        return filtered_users
     finally:
         await prisma.disconnect()
 
@@ -53,7 +62,16 @@ async def get_matched_users(request: Request):
         matched_user_ids = [match.user1_id if match.user2_id == user_id else match.user2_id for match in matches]
         matched_users = await prisma.users.find_many(where={'user_id': {'in': matched_user_ids}})
 
-        return matched_users
+        filtered_users = [
+            {
+            'user_id': user.user_id,
+            'profile_picture': user.profile_picture,
+            'name': user.name
+            }
+            for user in matched_users
+        ]
+        return filtered_users
+
     except Exception as e:
         print(f"Error fetching matched users: {e}")
         raise HTTPException(status_code=500, detail="Error fetching matched users")
